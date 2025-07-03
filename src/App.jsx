dafe8c4cadf200fde6795514422115ca1caebfcf
karl <karl.fahlbusch7241@gmail.com>
2025-07-03 12:43:17 -0500

    ain

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,56 @@
+import { useState } from "react";
+import { episodeList } from "./data";
+import "./index.css";
+
 export default function App() {
   // TODO
+  const [episodes] = useState(episodeList);
+  const [selectedEpisode, setSelectedEpisode] = useState();
+
+  function EpisodeDetails() {
+    if (!selectedEpisode) {
+      return (
+        <section className="details">
+          <h2>Episode Details</h2>
+          <p>Select an episode to learn more.</p>
+        </section>
+      );
+    }
+
+    return (
+      <section className="details">
+        <h2>
+          Episode {selectedEpisode.number}: {selectedEpisode.title}
+        </h2>
+        <p>{selectedEpisode.description}.</p>
+      </section>
+    );
+  }
+
+  function Episodes() {
+    return (
+      <section className="episodes">
+        <h2>Episode List</h2>
+        <ul className="episodes">
+          {episodes.map((episode) => (
+            <li key={episode.id} onClick={() => setSelectedEpisode(episode)}>
+              {episode.title}
+            </li>
+          ))}
+        </ul>
+      </section>
+    );
+  }
+
+  return (
+    <>
+      <header>
+        <h1>Dark Echoes</h1>
+      </header>
+      <main>
+        <Episodes />
+        <EpisodeDetails />
+      </main>
+    </>
+  );
 }
